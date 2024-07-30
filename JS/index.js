393bd41e15d4981b83802967f92459430fee3dbb
ErivaldoChaves <erivas_12@hotmail.com>
2024-07-30 10:07:06 -0300

    implementando java script do cronômetro

--- JS/index.js
+++ JS/index.js
@@ -7,8 +7,8 @@ var intervalo
 
 //Botão iniciar
 function iniciar() {
-    // A cada 1000 milisegundos(ou 1 segundo a função counting será chamada)
-    intervalo = setInterval(counting, 1000)
+    // A cada 1000 milisegundos(ou 1 segundo a função contando será chamada)
+    intervalo = setInterval(contando, 1000)
 }
 //Botão pausar
 function pausar(){
@@ -17,11 +17,38 @@ function pausar(){
 //Botão parar
 function parar(){
     clearInterval(intervalo)
-
+    //Zerando o contador
     horas = 0
     minutos = 0
     segundos = 0
-
-    window.document.getElementById('contador').innerText = `${twoDigits(horas)}:${twoDigits(minutos)}:${twoDigits(segundos)}`
+    // Exibindo a contagem
+    window.document.getElementById('contador').innerText = `${doisDigitos(horas)}:${doisDigitos(minutos)}:${doisDigitos(segundos)}`
 }
+//Contando
+function contando(){
+    //incrementando segundos
+    segundos ++
+    // A cada 60 segundos, minutos serão incrementados e segundos zerados
+    if(segundos === 60){
+        segundos = 0
+        minutos ++
+    }
+    // A cada 60 minutos, horas serão incrementadas e minutos zerados
+        if (minutos === 60){
+            minutos = 0
+            horas ++
+        }
+    // Exibindo a contagem
+    window.document.getElementById('contador').innerText = `${doisDigitos(horas)}:${doisDigitos(minutos)}:${doisDigitos(segundos)}`
+}
+// Mantendo a contagem em dois digitos
+function doisDigitos(digito){
+    if(digito < 10) {
+        return ('0' + digito)
+    }else{
+        return (digito)
+    }
+}
+
+
 
